37277f2bb4ef7956bce1fae3762e7f026ab2e1ef
joão morais <jhmorais.09@gmail.com>
2023-12-09 15:28:03 +0000

    add post

--- api/gabi/test.go
+++ api/gabi/test.go
@@ -11,9 +11,15 @@ func HandlerGabiTest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>You SUCCESS DEPLOY GOLANG page Gabi Test</h1>")
 }
 
+func HandlerGabiTestPost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "{'message': 'OK'}")
+}
+
 func Gabi() {
 	r := mux.NewRouter()
 	r.HandleFunc("/gabi/test", HandlerGabiTest).Methods("GET")
+	r.HandleFunc("/gabi/test", HandlerGabiTestPost).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
